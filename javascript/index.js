7b61d3e805177ba91ab1ce0b1f75149cc3b14186
Jesse Williamson <Jesse@Jesses-MacBook-Pro-2.local>
2017-08-17 07:18:55 -0700

    fixed a few things in the JS impl

--- javascript/index.js
+++ javascript/index.js
@@ -5,15 +5,6 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-// When ending, the game should first show a farewell message.
-// When continuing, the game should show prompt & await user input
-
-/*
-  Define a function to drive your game.
-  The function will need to take the current game state, the response
-  from the user and calculate the next game state.
-*/
-
 const loop = (gameData, prompt, run) => {
   rl.question(prompt, (resp) => {
     const { end, cont } = run(gameData, resp);
@@ -27,9 +18,19 @@ const loop = (gameData, prompt, run) => {
   });
 }
 
-const main = () => {
-  console.log('Running the loop');
-  loop(initGameState, "You ready?", runIt)
-}
+/*
+  Define a function to drive your game.
+  The function will need to take the current game state, the response
+  from the user and calculate the next game state.
 
-module.exports = main();
+  Pass that function, along with some initial data and prompt to
+  the loop function.
+
+  When ending, the game should first show a farewell message.
+  When continuing, the game should show prompt & await user input
+*/
+
+
+module.exports = () => {
+  console.log('do something like: `loop(initData, initPrompt, runFn)`');
+}
